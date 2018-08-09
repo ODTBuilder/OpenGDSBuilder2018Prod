9ca242b7089a6aec885c6b08d14cb157239b4429
SOYIJUN <monkey1221@git.co.kr>
2018-08-09 13:25:14 +0900

    rabbit mq setting change

--- opengds2018prod/src/main/java/com/gitrnd/qaproducer/common/worker/Producer.java
+++ opengds2018prod/src/main/java/com/gitrnd/qaproducer/common/worker/Producer.java
@@ -3,7 +3,6 @@ package com.gitrnd.qaproducer.common.worker;
 import org.json.simple.JSONObject;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,21 +10,17 @@ public class Producer {
 
 	@Autowired
 	private RabbitTemplate template;
-	@Value("${gitrnd.rabbitmq.exchange}")
-	private String exchange;
-	@Value("${gitrnd.rabbitmq.routingKey}")
-	private String routingKey;
 
 	public void produceMsg(String msg) {
 		System.out.println("Send msg = " + msg);
-		template.convertSendAndReceive(exchange, routingKey, msg);
+		template.convertSendAndReceive(msg);
 	}
 
 	public JSONObject produceMobileMsg(String msg) {
 
 		System.out.println("Send msg = " + msg);
 		template.setReplyTimeout(Long.MAX_VALUE);
-		JSONObject response = (JSONObject) template.convertSendAndReceive(exchange, routingKey, msg);
+		JSONObject response = (JSONObject) template.convertSendAndReceive(msg);
 		System.out.println(response);
 
 		return response;

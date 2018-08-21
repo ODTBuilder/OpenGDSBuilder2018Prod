deb94b2610e900aeb64810ca61cb46e709150f78
SOYIJUN <monkey1221@git.co.kr>
2018-08-21 10:39:01 +0900

    layer def trim

--- opengds2018prod/src/main/resources/static/resources/js/gb/embed/layerdefinition.js
+++ opengds2018prod/src/main/resources/static/resources/js/gb/embed/layerdefinition.js
@@ -155,7 +155,8 @@ gb.embed.LayerDefinition = function(obj) {
 			}
 			reader.readAsText(fileList[0]);
 			$(reader).on("load", function(event) {
-				var obj = JSON.parse(reader.result.replace(/(\s*)/g, ''));
+				var obj = JSON.parse(reader.result);
+				// var obj = JSON.parse(reader.result.replace(/(\s*)/g, ''));
 				var flag = that.setStructure(obj);
 				that.updateStructure();
 			});
@@ -577,11 +578,22 @@ gb.embed.LayerDefinition.prototype.inputAttributeValues = function(inp) {
 			if (Array.isArray(fix)) {
 				fixElem = fix[attrIdx];
 				if (fixElem !== undefined && fixElem !== null) {
-					fixElem["values"] = $(inp).val().replace(/(\s*)/g, '').split(",");
+					fixElem["values"] = $(inp).val().split(",");
+					var attrValues = fixElem["values"];
+					for (var z = 0; z < attrValues.length; z++) {
+						attrValues[z] = attrValues[z].trim();
+					}
+					// fixElem["values"] = $(inp).val().replace(/(\s*)/g,
+					// '').split(",");
 				} else {
 					var obj = {
-						"values" : $(inp).val().replace(/(\s*)/g, '').split(",")
+						"values" : $(inp).val().split(",")
+					// "values" : $(inp).val().replace(/(\s*)/g, '').split(",")
 					};
+					var attrValues = obj.values;
+					for (var z = 0; z < attrValues.length; z++) {
+						attrValues[z] = attrValues[z].trim();
+					}
 				}
 			}
 		}

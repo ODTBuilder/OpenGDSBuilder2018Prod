8f8e07e717e62e19af5275facb5506fdc20306e6
DAYEONOH <dayeonoh@git.co.kr>
2018-11-06 13:35:12 +0900

    commit

--- opengds2018prod/src/main/java/com/gitrnd/qaproducer/geogig/service/GeogigFeatureServiceImpl.java
+++ opengds2018prod/src/main/java/com/gitrnd/qaproducer/geogig/service/GeogigFeatureServiceImpl.java
@@ -106,7 +106,7 @@ public class GeogigFeatureServiceImpl implements GeogigFeatureService {
 			for (int i = 0; i < commitSize; i++) {
 				Commit newCommit = commits.get(i); // current
 				SimpleCommit simpleCommit = new SimpleCommit();
-				simpleCommit.setcIdx(index + i); // idx
+				simpleCommit.setcIdx(index); // idx
 				String commitId = newCommit.getCommitId(); // commit id
 				simpleCommit.setCommitId(commitId);
 				simpleCommit.setAuthorName(newCommit.getAuthor().getName()); // author

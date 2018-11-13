2aa92edf96e58f3af4e36318615121072f5d36b9
SOYIJUN <monkey1221@git.co.kr>
2018-11-13 15:21:28 +0900

    geoserver address change to 32

--- opengds2018prod/src/main/resources/static/resources/js/gb/tree/geoserver.js
+++ opengds2018prod/src/main/resources/static/resources/js/gb/tree/geoserver.js
@@ -784,8 +784,8 @@ gb.tree.GeoServer.prototype.openAddGeoServer = function() {
 		that.addGeoServer($(gNameInput).val(), $(gURLInput).val(), $(gIDInput).val(), $(gPassInput).val(), addGeoServerModal);
 	});
 
-	gNameInput.val("geoserver42");
-	gURLInput.val("http://175.116.181.42:9990/geoserver");
+	gNameInput.val("geoserver32");
+	gURLInput.val("http://175.116.181.32:9999/geoserver");
 	gIDInput.val("admin");
 	gPassInput.val("geoserver");
 };

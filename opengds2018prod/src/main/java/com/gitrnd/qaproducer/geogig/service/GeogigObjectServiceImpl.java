48a01b8fb07bd0ee7e32bb8a2d0d232849aa910e
DAYEONOH <dayeonoh@git.co.kr>
2018-11-14 10:48:45 +0900

    feature diff 수정

--- opengds2018prod/src/main/java/com/gitrnd/qaproducer/geogig/service/GeogigObjectServiceImpl.java
+++ opengds2018prod/src/main/java/com/gitrnd/qaproducer/geogig/service/GeogigObjectServiceImpl.java
@@ -14,11 +14,16 @@ import org.springframework.stereotype.Service;
 
 import com.gitrnd.gdsbuilder.geogig.GeogigCommandException;
 import com.gitrnd.gdsbuilder.geogig.command.object.CatObject;
+import com.gitrnd.gdsbuilder.geogig.command.repository.LsTreeRepository;
 import com.gitrnd.gdsbuilder.geogig.type.GeogigCat;
 import com.gitrnd.gdsbuilder.geogig.type.GeogigCat.Attribute;
+import com.gitrnd.gdsbuilder.geogig.type.GeogigCat.Commit;
 import com.gitrnd.gdsbuilder.geogig.type.GeogigCat.Feature;
+import com.gitrnd.gdsbuilder.geogig.type.GeogigCat.FeatureType;
 import com.gitrnd.gdsbuilder.geogig.type.GeogigCat.Subtree;
 import com.gitrnd.gdsbuilder.geogig.type.GeogigFeatureAttribute;
+import com.gitrnd.gdsbuilder.geogig.type.GeogigRevisionTree;
+import com.gitrnd.gdsbuilder.geogig.type.GeogigRevisionTree.Node;
 import com.gitrnd.gdsbuilder.geoserver.DTGeoserverManager;
 
 /**
@@ -49,8 +54,8 @@ public class GeogigObjectServiceImpl implements GeogigObjectService {
 	}
 
 	@Override
-	public GeogigFeatureAttribute catFeatureObject(DTGeoserverManager geoserverManager, String repoName, String path,
-			String commitId, String featureId) throws JAXBException {
+	public GeogigFeatureAttribute catConflictFeatureObject(DTGeoserverManager geoserverManager, String repoName,
+			String path, String commitId, String featureId) throws JAXBException {
 
 		String url = geoserverManager.getRestURL();
 		String user = geoserverManager.getUsername();
@@ -64,29 +69,79 @@ public class GeogigObjectServiceImpl implements GeogigObjectService {
 			CatObject catObj = new CatObject();
 			List<Attribute> attrTypes = null;
 			GeogigCat catOurs = catObj.executeCommand(url, user, pw, repoName, commitId);
-			String treeId = catOurs.getCommit().getTree();
-			GeogigCat catTree = catObj.executeCommand(url, user, pw, repoName, treeId);
-			List<Subtree> subTrees = catTree.getTree().getSubtree();
-			for (Subtree subTree : subTrees) {
-				if (subTree.getName().equals(layerName)) {
-					String metaDataObjId = subTree.getMetadataid();
-					GeogigCat catFeatureType = catObj.executeCommand(url, user, pw, repoName, metaDataObjId);
-					attrTypes = catFeatureType.getFeaturetype().getAttribute();
+			Commit commit = catOurs.getCommit();
+			if (commit != null) {
+				String treeId = catOurs.getCommit().getTree();
+				GeogigCat catTree = catObj.executeCommand(url, user, pw, repoName, treeId);
+				List<Subtree> subTrees = catTree.getTree().getSubtree();
+				for (Subtree subTree : subTrees) {
+					if (subTree.getName().equals(layerName)) {
+						String metaDataObjId = subTree.getMetadataid();
+						GeogigCat catFeatureType = catObj.executeCommand(url, user, pw, repoName, metaDataObjId);
+						attrTypes = catFeatureType.getFeaturetype().getAttribute();
+					}
+				}
+				if (attrTypes != null) {
+					GeogigCat catFeature = catObj.executeCommand(url, user, pw, repoName, featureId);
+					Feature feature = catFeature.getFeature();
+					List<Attribute> attrValues = feature.getAttribute();
+					for (int i = 0; i < attrTypes.size(); i++) {
+						Attribute type = attrTypes.get(i);
+						type.setValue(attrValues.get(i).getValue());
+					}
+					featureAtt = new GeogigFeatureAttribute();
+					featureAtt.setLayerName(layerName);
+					featureAtt.setFeatureId(featureName);
+					featureAtt.setAttributes(attrTypes);
+					featureAtt.setSuccess("true");
 				}
 			}
-			if (attrTypes != null) {
-				GeogigCat catFeature = catObj.executeCommand(url, user, pw, repoName, featureId);
-				Feature feature = catFeature.getFeature();
-				List<Attribute> attrValues = feature.getAttribute();
-				for (int i = 0; i < attrTypes.size(); i++) {
-					Attribute type = attrTypes.get(i);
-					type.setValue(attrValues.get(i).getValue());
+		} catch (GeogigCommandException e) {
+			JAXBContext jaxbContext = JAXBContext.newInstance(GeogigFeatureAttribute.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			featureAtt = (GeogigFeatureAttribute) unmarshaller.unmarshal(new StringReader(e.getMessage()));
+		}
+		return featureAtt;
+	}
+
+	@Override
+	public GeogigFeatureAttribute catFeatureObject(DTGeoserverManager geoserverManager, String repoName, String path,
+			String objectid) throws JAXBException {
+
+		String url = geoserverManager.getRestURL();
+		String user = geoserverManager.getUsername();
+		String pw = geoserverManager.getPassword();
+
+		String[] ids = path.split("/");
+		String layerName = ids[0];
+		String featureName = ids[1];
+		GeogigFeatureAttribute featureAtt = null;
+		try {
+			CatObject catObj = new CatObject();
+			GeogigCat geogigCat = catObj.executeCommand(url, user, pw, repoName, objectid);
+			Feature feature = geogigCat.getFeature();
+			List<Attribute> attrValues = feature.getAttribute();
+			if (attrValues != null) {
+				LsTreeRepository lsTreeRepos = new LsTreeRepository();
+				GeogigRevisionTree geogigLsTree = lsTreeRepos.executeCommand(url, user, pw, repoName, path, true);
+				List<Node> nodes = geogigLsTree.getNodes();
+				for (Node node : nodes) {
+					if (path.equalsIgnoreCase(node.getPath()) && objectid.equalsIgnoreCase(node.getObjectId())) {
+						String metaId = node.getMetadataId();
+						GeogigCat geogigCatMeta = catObj.executeCommand(url, user, pw, repoName, metaId);
+						FeatureType featureType = geogigCatMeta.getFeaturetype();
+						List<Attribute> attrTypes = featureType.getAttribute();
+						for (int i = 0; i < attrTypes.size(); i++) {
+							Attribute type = attrTypes.get(i);
+							type.setValue(attrValues.get(i).getValue());
+						}
+						featureAtt = new GeogigFeatureAttribute();
+						featureAtt.setLayerName(layerName);
+						featureAtt.setFeatureId(featureName);
+						featureAtt.setAttributes(attrTypes);
+						featureAtt.setSuccess("true");
+					}
 				}
-				featureAtt = new GeogigFeatureAttribute();
-				featureAtt.setLayerName(layerName);
-				featureAtt.setFeatureId(featureName);
-				featureAtt.setAttributes(attrTypes);
-				featureAtt.setSuccess("true");
 			}
 		} catch (GeogigCommandException e) {
 			JAXBContext jaxbContext = JAXBContext.newInstance(GeogigFeatureAttribute.class);

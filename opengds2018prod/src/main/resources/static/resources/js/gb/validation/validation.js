5cb8902007e17e93ef8b512cc9062eeeed3bfc78
hochul-PC\hochul <hochul1005@naver.com>
2018-11-21 15:48:39 +0900

    edit tool 알림창 수정 && layer style 기능 수정 && web 검수 요청 기능 추가

--- opengds2018prod/src/main/resources/static/resources/js/gb/validation/validation.js
+++ opengds2018prod/src/main/resources/static/resources/js/gb/validation/validation.js
@@ -13,6 +13,40 @@ if (!gb.validation)
 	gb.validation = {};
 
 (function($){
+	
+	let presetIndex_ = 0;
+	
+	const INDEXLIST = {
+		"Digital Map 1.0" : 1,
+		"Digital Map 2.0" : 1,
+		"Underground Map 1.0" : 2,
+		"Underground Map 2.0" : 2,
+		"Forest Map" : 2
+	};
+	
+	const QATYPE = {
+		"1": {
+			"ver": "qa1",
+			"type": "nm5"
+		},
+		"2": {
+			"ver": "qa2",
+			"type": "nm5"
+		},
+		"3": {
+			"ver": "qa1",
+			"type": "ug5"
+		},
+		"4": {
+			"ver": "qa2",
+			"type": "ug5"
+		},
+		"5": {
+			"ver": "qa2",
+			"type": "fr5"
+		},
+	};
+	
 	gb.validation.Validation = function(obj) {
 		obj.keep = true;
 		gb.modal.Base.call(this, obj);
@@ -20,7 +54,7 @@ if (!gb.validation)
 		
 		var options = obj || {};
 	
-		this.token = options.token || "";
+		this.url = options.url || "";
 		
 		this.geoserverTree = undefined;
 		this.workingTree = undefined;
@@ -32,7 +66,7 @@ if (!gb.validation)
 		this.formatSelectTag.append($("<option>").text("Select File Format:"));
 		
 		this.srsSelectTag = $("<select class='form-control'>");
-		this.srsSelectTag.append($("<option>").text("Select Coordinate System:"));
+		this.srsSelectTag.append($("<option>").text("좌표계를 선택해주세요:"));
 		this.srsSelectTag.append($("<option>").val("5186").text("중부원점 투영좌표계(Central Belt 2010)"));
 		this.srsSelectTag.append($("<option>").val("4737").text("GRS80 경위도 좌표계(Korean 2000)"));
 		this.srsSelectTag.append($("<option>").val("4326").text("WGS84 경위도 좌표계(전지구 좌표계)"));
@@ -128,6 +162,53 @@ if (!gb.validation)
 	gb.validation.Validation.prototype = Object.create(gb.modal.Base.prototype);
 	gb.validation.Validation.prototype.constructor = gb.validation.Validation;
 	
+	gb.validation.Validation.prototype.getParameter = function(){
+		var params = {};
+		var datastoreList = {};
+		var node;
+		var preset = this.presetSelectTag.find("option:selected");
+		var srs = this.srsSelectTag.find("option:selected");
+		var list = this.workingTree.get_node("#").children_d || [];
+		
+		params.geoserver = {};
+		params.geoserver.layers = {};
+		
+		for(var i = 0; i < list.length; i++){
+			node = this.workingTree.get_node(list[i]);
+			if(node.type === "geoserver"){
+				params.geoserver.servername = node.text;
+			} else if(node.type === "workspace"){
+				params.geoserver.layers[node.text] = [];
+			} else if(node.type === "datastore"){
+				datastoreList[node.id] = this.workingTree.get_node(node.parent).text;
+			} else {
+				params.geoserver.layers[datastoreList[node.parent]].push(node.text);
+			}
+		}
+		
+		if(!srs.val()){
+			this.messageContent.html("좌표계를 선택해야합니다.");
+		} else {
+			params.crs = "EPSG:" + srs.val();
+		}
+		
+		if(!preset.data("prid")){
+			this.messageContent.html("Preset ID가 없습니다.");
+		} else {
+			params.prid = preset.data("prid");
+		}
+		
+		if(!preset.data("cat")){
+			this.messageContent.html("Category ID가 없습니다.");
+		} else {
+			params.cat = preset.data("cat");
+			params.qaVer = QATYPE[preset.data("cat").toString()].ver;
+			params.qaType = QATYPE[preset.data("cat").toString()].type;
+		}
+		
+		return params;
+	}
+	
 	gb.validation.Validation.prototype.createStepDiv = function(){
 		var icon1 = $("<i>");
 		var title1 = $("<h2 class='step-title'>");
@@ -219,14 +300,14 @@ if (!gb.validation)
 			"height" : "502px",
 			"url" : {
 				"getTree" : "geoserver/getGeolayerCollectionTree.ajax"
-						+ this.token,
-				"addGeoServer" : "geoserver/addGeoserver.ajax" + this.token,
+						+ this.url.token,
+				"addGeoServer" : "geoserver/addGeoserver.ajax" + this.url.token,
 				"deleteGeoServer" : "geoserver/removeGeoserver.ajax"
-						+ this.token,
+						+ this.url.token,
 				"getMapWMS" : "geoserver/geoserverWMSGetMap.ajax"
-						+ this.token,
+						+ this.url.token,
 				"getLayerInfo" : "geoserver/getGeoLayerInfoList.ajax"
-						+ this.token
+						+ this.url.token
 			}
 		});
 		
@@ -263,23 +344,55 @@ if (!gb.validation)
 				.append("Add");
 		
 		panelFooter.on("click", function(e){
-			var i;
+			var i, arr, children;
 			var tree = that.geoserverTree;
 			var workTree = that.workingTree;
 			var selects = tree.get_selected(true);
 			
 			for(i = 0; i < selects.length; i++){
-				if(!workTree.get_node(selects[i].id)){
-					if(!workTree.get_node(selects[i].parent)){
-						workTree.create_node("#", selects[i].original, "last", false, false);
-					} else {
-						workTree.create_node(selects[i].parent, selects[i].original, "last", false, false);
+				if(selects[i].parents instanceof Array){
+					arr = selects[i].parents.slice();
+				}
+				
+				// 선택된 node의 부모 node들을 전부 추가
+				for(var j = arr.shift(); j !== undefined; j = arr.shift()){
+					if(j === "#"){
+						continue;
 					}
 					
-					if(selects[i].children.length > 0){
-						updateChildren(tree, workTree, selects[i].children);
+					if(!workTree.get_node(j)){
+						workTree.create_node("#", tree.get_node(j).original, "last", false, false);
+					}
+					
+					if(tree.get_node(j).children.length > 0){
+						updateChildren(tree, workTree, tree.get_node(j).children);
+					}
+					
+					if(tree.get_node(j).type === "workspace" && presetIndex_ === 2){
+						removeSameTypeNode(tree.get_node(j), workTree, "#");
+					}
+					
+					if(tree.get_node(j).type === "geoserver" && (presetIndex_ === 1 || presetIndex_ === 2)){
+						removeSameTypeNode(tree.get_node(j), workTree, "#");
 					}
 				}
+				
+				// 선택된 node 추가
+				if(!workTree.get_node(selects[i].id)){
+					workTree.create_node(selects[i].parent, selects[i].original, "last", false, false);
+				}
+				
+				if(selects[i].type === "workspace" && presetIndex_ === 2){
+					removeSameTypeNode(selects[i], workTree, "#");
+				}
+				
+				if(selects[i].type === "geoserver" && (presetIndex_ === 1 || presetIndex_ === 2)){
+					removeSameTypeNode(selects[i], workTree, "#");
+				}
+				
+				children = selects[i].children || [];
+				// 선택된 node의 자식 node들을 전부 추가
+				createChildren(tree, workTree, children);
 			}
 			
 			workTree.open_all();
@@ -290,6 +403,45 @@ if (!gb.validation)
 		return treeContent;
 	}
 	
+	function removeSameTypeNode(node, workingTree, root){
+		var node = node;
+		var workTree = workingTree;
+		var workNode = workTree.get_node(root);
+		var children = workNode.children || [];
+		
+		if(!!workNode){
+			if(node.type === workNode.type && node.id !== workNode.id){
+				workTree.delete_node(workNode);
+			} else {
+				for(var i = 0; i < children.length; i++){
+					removeSameTypeNode(node, workTree, children[i])
+				}
+			}
+		}
+	}
+	
+	function createChildren(geoTree, workingTree, children){
+		var i, node;
+		var tree = geoTree;
+		var workTree = workingTree;
+		var list = children;
+		for(i = 0; i < list.length; i++){
+			node = tree.get_node(list[i]);
+			if(!workTree.get_node(list[i])){
+				workTree.create_node(tree.get_node(list[i]).parent, node.original, "last", false, false);
+			}
+			if(node.children instanceof Array){
+				// 자식 노드가 자식 노드를 가지고 있다면 callback 실행
+				createChildren(tree, workTree, node.children);
+			}
+			
+			if(node.type === "workspace" && presetIndex_ === 2){
+				break;
+			}
+		}
+	}
+	
+	// work tree의 자식 노드들을 적절한 부모 노드 아래로 이동
 	function updateChildren(geoTree, workingTree, children){
 		var i, node;
 		var tree = geoTree;
@@ -298,12 +450,6 @@ if (!gb.validation)
 		for(i = 0; i < list.length; i++){
 			node = tree.get_node(list[i]);
 			if(!workTree.get_node(list[i])){
-				if(!workTree.get_node(node.parent)){
-					workTree.create_node("#", node.original, "last", false, false);
-				} else {
-					workTree.create_node(node.parent, node.original, "last", false, false);
-				}
-				
 				if(node.children.length > 0){
 					updateChildren(tree, workTree, node.children);
 				}
@@ -436,13 +582,13 @@ if (!gb.validation)
 		var i = $("<div>").addClass("fas fa-info-circle fa-2x");
 		this.messageContent = $("<p>").css({
 			"margin": "0 3px"
-		}).html("User Setting을 먼저 선택해주세요.");
+		}).html("사용자 정의 옵션을 먼저 선택해주세요.");
 		var message = $("<div>").addClass("validation-message").append(i).append(this.messageContent);
 		var body = 
 			$("<div>")
 				.append(presetDiv).append(srsDiv).append(message);
 		
-		requestPreset(this.presetSelectTag, presetDiv);
+		requestPreset(this, this.presetSelectTag, presetDiv, this.messageContent);
 		//customSelect(formatDiv);
 		customSelect(srsDiv);
 		
@@ -482,8 +628,28 @@ if (!gb.validation)
 				.addClass("gb-button-primary")
 				.text("Start")
 				.click(function(e) {
-					alert("검수 요청이 완료되었습니다. 검수 결과 페이지에서 진행 상황을 확인할 수 있습니다.");
-					that.close();
+					var params = that.getParameter();
+					
+					var deferredObj = 
+						$.ajax({
+						url : that.url.requestValidate + that.url.token,
+						type : "POST",
+						contentType : "application/json; charset=UTF-8",
+						dataType : "json",
+						cache : false,
+						data : params,
+						traditional: true
+					});
+					
+					deferredObj.done(function(data, textStatus, jqXHR) {
+						alert("검수 요청이 완료되었습니다. 검수 결과 페이지에서 진행 상황을 확인할 수 있습니다.");
+						that.close();
+					});
+					
+					deferredObj.fail(function(jqXHR, textStatus, errorThrown) {
+						alert("검수 요청이 실패했습니다.");
+						that.close();
+					});
 				});
 	
 		var buttonArea = 
@@ -495,26 +661,36 @@ if (!gb.validation)
 		return buttonArea;
 	}
 	
-	function requestPreset(obj, div){
+	function requestPreset(that, obj, div, message){
 		var select = obj;
 		var div = div;
+		var message = message;
 		$.ajax({
 			url : "option/retrievePresetByUidx.ajax",
 			method : "GET",
 			success : function(data, textStatus, jqXHR) {
-				var option = $("<option>").val("false").text("Select User Setting:");
+				var option = $("<option>").val("false").text("사용자 정의 옵션을 선택해주세요:");
 				select.append(option);
 				for (var i = 0; i < data.length; i++) {
-					option = $("<option>").val(data[i].pid).attr("data-title", data[i].title).text(data[i].name);
+					option = 
+						$("<option>")
+							.val(data[i].pid)
+							.attr("data-title", data[i].title)
+							.attr("data-cat", data[i].cat)
+							.attr("data-prid", data[i].pid)
+							.text(data[i].name);
+					
 					select.append(option);
+					
 				}
-				customSelect(div);
+				customSelect(div, message, that);
 			}
 		});
 	}
 	
-	function customSelect(selectDiv){
+	function customSelect(selectDiv, message, that){
 		var x, j, selElmnt, a, b, c;
+		var that = that;
 		/*look for any elements with the class "custom-select":*/
 		x = selectDiv;
 		selElmnt = x.find("select")[0];
@@ -544,9 +720,24 @@ if (!gb.validation)
 							y[k].removeAttribute("class");
 						}
 						this.setAttribute("class", "same-as-selected");
-						if(!!s.options[i].dataset.support){
-							list = s.options[i].dataset.support.trim().split(";");
-							updateFormatSelect(list);
+						if(!!s.options[i].dataset.title){
+							if(!!INDEXLIST[s.options[i].dataset.title]){
+								if(INDEXLIST[s.options[i].dataset.title] === 1){
+									message.html(s.options[i].dataset.title + "는 workspace를 여러개 선택할 수 있습니다.");
+								} else if(INDEXLIST[s.options[i].dataset.title] === 2){
+									message.html(s.options[i].dataset.title + "는 workspace를 하나만 선택할 수 있습니다.");
+								}
+								if(presetIndex_ !== undefined && presetIndex_ !== null){
+									if(presetIndex_ !== INDEXLIST[s.options[i].dataset.title]){
+										var arr = that.workingTree.get_node("#").children || [];
+										for(var t = 0; t < arr.length; t++){
+											that.workingTree.delete_node(arr[t]);
+										}
+									}
+									presetIndex_ = INDEXLIST[s.options[i].dataset.title];
+								}
+							}
+							
 						}
 						break;
 					}

3a1c7182a576609025f91c04211ea09b51e33ca6
SOYIJUN <monkey1221@git.co.kr>
2019-06-28 17:14:46 +0900

    map 보완

--- opengds2018prod/src/main/resources/static/resources/js/gb/map/map.js
+++ opengds2018prod/src/main/resources/static/resources/js/gb/map/map.js
@@ -97,72 +97,42 @@ gb.Map = function(obj) {
 		"position" : "relative"
 	});
 
-	this.upperMap = new ol.Map(
-			{
-				controls : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("controls") ? options.upperMap.controls
-						: undefined : undefined,
-				pixelRatio : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("pixelRatio") ? options.upperMap.pixelRatio
-						: undefined
-						: undefined,
-				interactions : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("interactions") ? options.upperMap.interactions
-						: undefined
-						: undefined,
-				keyboardEventTarget : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("keyboardEventTarget") ? options.upperMap.keyboardEventTarget
-						: undefined
-						: undefined,
-				layers : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("layers") ? options.upperMap.layers
-						: undefined : undefined,
-				maxTilesLoading : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("maxTilesLoading") ? options.upperMap.maxTilesLoading
-						: undefined
-						: undefined,
-				loadTilesWhileAnimating : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("loadTilesWhileAnimating") ? options.upperMap.loadTilesWhileAnimating
-						: undefined
-						: undefined,
-				loadTilesWhileInteracting : options.hasOwnProperty("upperMap") ? options.upperMap
-						.hasOwnProperty("loadTilesWhileInteracting") ? options.upperMap.loadTilesWhileInteracting : undefined : undefined,
-				moveTolerance : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("moveTolerance") ? options.upperMap.moveTolerance
-						: undefined
-						: undefined,
-				overlays : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("overlays") ? options.upperMap.overlays
-						: undefined : undefined,
-				target : this.upperDiv[0],
-				view : this.view
-			});
-	this.lowerMap = new ol.Map(
-			{
-				controls : options.hasOwnProperty("upperMap") ? options.lowerMap.hasOwnProperty("controls") ? options.lowerMap.controls
-						: undefined : undefined,
-				pixelRatio : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("pixelRatio") ? options.lowerMap.pixelRatio
-						: undefined
-						: undefined,
-				interactions : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("interactions") ? options.lowerMap.interactions
-						: undefined
-						: undefined,
-				keyboardEventTarget : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("keyboardEventTarget") ? options.lowerMap.keyboardEventTarget
-						: undefined
-						: undefined,
-				layers : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("layers") ? options.lowerMap.layers
-						: undefined : undefined,
-				maxTilesLoading : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("maxTilesLoading") ? options.lowerMap.maxTilesLoading
-						: undefined
-						: undefined,
-				loadTilesWhileAnimating : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("loadTilesWhileAnimating") ? options.lowerMap.loadTilesWhileAnimating
-						: undefined
-						: undefined,
-				loadTilesWhileInteracting : options.hasOwnProperty("lowerMap") ? options.lowerMap
-						.hasOwnProperty("loadTilesWhileInteracting") ? options.lowerMap.loadTilesWhileInteracting : undefined : undefined,
-				moveTolerance : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("moveTolerance") ? options.lowerMap.moveTolerance
-						: undefined
-						: undefined,
-				overlays : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("overlays") ? options.lowerMap.overlays
-						: undefined : undefined,
-				target : this.lowerDiv[0],
-				view : this.view
-			});
+	this.upperMap = new ol.Map({
+		controls : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("controls") ? options.upperMap.controls : undefined : undefined,
+		pixelRatio : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("pixelRatio") ? options.upperMap.pixelRatio : undefined : undefined,
+		interactions : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("interactions") ? options.upperMap.interactions : undefined : undefined,
+		keyboardEventTarget : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("keyboardEventTarget") ? options.upperMap.keyboardEventTarget : undefined : undefined,
+		layers : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("layers") ? options.upperMap.layers : undefined : undefined,
+		maxTilesLoading : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("maxTilesLoading") ? options.upperMap.maxTilesLoading : undefined : undefined,
+		loadTilesWhileAnimating : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("loadTilesWhileAnimating") ? options.upperMap.loadTilesWhileAnimating : undefined : undefined,
+		loadTilesWhileInteracting : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("loadTilesWhileInteracting") ? options.upperMap.loadTilesWhileInteracting : undefined
+				: undefined,
+		moveTolerance : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("moveTolerance") ? options.upperMap.moveTolerance : undefined : undefined,
+		overlays : options.hasOwnProperty("upperMap") ? options.upperMap.hasOwnProperty("overlays") ? options.upperMap.overlays : undefined : undefined,
+		target : this.upperDiv[0],
+		view : this.view
+	});
+	this.lowerMap = new ol.Map({
+		controls : options.hasOwnProperty("upperMap") ? options.lowerMap.hasOwnProperty("controls") ? options.lowerMap.controls : undefined : undefined,
+		pixelRatio : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("pixelRatio") ? options.lowerMap.pixelRatio : undefined : undefined,
+		interactions : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("interactions") ? options.lowerMap.interactions : undefined : undefined,
+		keyboardEventTarget : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("keyboardEventTarget") ? options.lowerMap.keyboardEventTarget : undefined : undefined,
+		layers : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("layers") ? options.lowerMap.layers : undefined : undefined,
+		maxTilesLoading : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("maxTilesLoading") ? options.lowerMap.maxTilesLoading : undefined : undefined,
+		loadTilesWhileAnimating : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("loadTilesWhileAnimating") ? options.lowerMap.loadTilesWhileAnimating : undefined : undefined,
+		loadTilesWhileInteracting : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("loadTilesWhileInteracting") ? options.lowerMap.loadTilesWhileInteracting : undefined
+				: undefined,
+		moveTolerance : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("moveTolerance") ? options.lowerMap.moveTolerance : undefined : undefined,
+		overlays : options.hasOwnProperty("lowerMap") ? options.lowerMap.hasOwnProperty("overlays") ? options.lowerMap.overlays : undefined : undefined,
+		target : this.lowerDiv[0],
+		view : this.view
+	});
 
 	$(this.lowerDiv).find(".ol-viewport").css("z-index", 1);
 	// 윗 지도가 아래 지도보다 우선적으로 보여지도록 인덱스 설정
 	$(this.upperDiv).find(".ol-viewport").css("z-index", 2);
+	// 지도 영역 DIV 크기에 맞춰 지도 크기 업데이트
+	this.updateSize();
 };
 /**
  * 상위 지도 영역 ol.Map 객체를 반환한다.
@@ -259,4 +229,23 @@ gb.Map.prototype.setSize = function(width, height) {
 		"position" : "relative"
 	});
 
+};
+
+/**
+ * 지도 영역의 크기를 현재 DIV 크기에 맞춰 업데이트한다.
+ * 
+ * @method gb.Map#updateSize
+ */
+gb.Map.prototype.updateSize = function() {
+	this.setSize($(this.bind).width(), $(this.bind).height());
+};
+
+/**
+ * 두 지도가 공유하는 ol.View 객체를 반환한다.
+ * 
+ * @method gb.Map#getView
+ * @return {ol.View} ol.View 객체
+ */
+gb.Map.prototype.getView = function() {
+	return this.view;
 };
